671a8aef913d29e48b061eea2ca0993d20729ad5
Orlando C. Patriarcha <orlando.patriarcha@gmail.com>
2020-04-29 17:39:22 -0300

    project init

---
+++ atividadesAula4/src/main/java/br/com/each/ppgsi/testesDeSoftware/App.java
@@ -0,0 +1,32 @@
+package br.com.each.ppgsi.testesDeSoftware;
+
+import org.jgrapht.generate.CompleteGraphGenerator;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    private static final int SIZE = 10;
+    
+
+    public void createCompleteGraph() {
+//        SimpleWeightedGraph completeGraph = new SimpleWeightedGraph<>(DefaultEdge.class);
+//        CompleteGraphGenerator<String, DefaultEdge> completeGenerator
+//                = new CompleteGraphGenerator<>(SIZE);
+//        VertexFactory<String> vFactory = new VertexFactory<String>() {
+//            private int id = 0;
+//
+//            public String createVertex() {
+//                return "v" + id++;
+//            }
+//        };
+//        completeGenerator.generateGraph(completeGraph, vFactory, null);
+    }
+}
